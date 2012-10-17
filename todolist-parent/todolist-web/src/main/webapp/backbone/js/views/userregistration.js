196da028404057b2ca6e4c6f2a0a5995845e1127
unknown <octavio.rosario@EZE3-DNN-00446.synapse.com>
2012-10-17 14:38:20 -0300

    user registration

--- todolist-parent/todolist-web/src/main/webapp/backbone/js/views/userregistration.js
+++ todolist-parent/todolist-web/src/main/webapp/backbone/js/views/userregistration.js
@@ -6,8 +6,7 @@ window.UserRegistrationView = Backbone.View.extend({
     },
 
     events: {
-//        "click #loginButton": "login",
-//        "click #createAcount": "createAcount",
+        "click #newaccount": "newaccount",
         "click #cancel": "cancel"
         
     },
@@ -16,44 +15,10 @@ window.UserRegistrationView = Backbone.View.extend({
     	 $(this.el).html(this.template());
         return this;
     },
-
-//    login:function (event) {
-//        event.preventDefault(); // Don't let this button submit the form
-//        $('.alert-error').hide(); // Hide any errors on a new submit
-//        var url = '../api/login';
-//        console.log('Loggin in... ');
-//        var formValues = {
-//            email: $('#inputEmail').val(),
-//            password: $('#inputPassword').val()
-//        };
-//
-//        $.ajax({
-//            url:url,
-//            type:'POST',
-//            dataType:"json",
-//            data: formValues,
-//            success:function (data) {
-//                console.log(["Login request details: ", data]);
-//               
-//                if(data.error) {  // If there is an error, show the error messages
-//                    $('.alert-error').text(data.error.text).show();
-//                }
-//                else { // If not, send them back to the home page
-//                    window.location.replace('#');
-//                }
-//            }
-//        });
-//    },
-    
-//	createAcount:function(event){
-//		event.preventDefault(); 
-//		window.location = "tpl/useregistration.html";
-//	}, 
 	
 	cancel:function(event){
         event.preventDefault(); // Don't let this button submit the form
         $('.alert-error').hide(); // Hide any errors on a new submit
-        var url = 'tpl/login.html';
         $.ajax({
         	
             url:url,
@@ -64,6 +29,34 @@ window.UserRegistrationView = Backbone.View.extend({
 
             }
         });
-	}
+	},
+    
+    newaccount:function(event) {
+//       event.preventDefault(); // Don't let this button submit the form
+//       $('.alert-error').hide(); // Hide any errors on a new submit
+//       var url = '../api/login';
+//       console.log('Loggin in... ');
+//       var formValues = {
+//          email: $('#inputEmail').val(),
+//          password: $('#inputPassword').val()
+//       };
+//
+//       $.ajax({
+//          url:url,
+//          type:'POST',
+//          dataType:"json",
+//          data: formValues,
+//          success:function (data) {
+//              console.log(["Login request details: ", data]);
+//             
+//              if(data.error) {  // If there is an error, show the error messages
+//                  $('.alert-error').text(data.error.text).show();
+//              }
+//              else { // If not, send them back to the home page
+//                  window.location.replace('#');
+//              }
+//          }
+//       });
+     }
     
 });
