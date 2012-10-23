8684fa69d569581bf50182c38edd26eabfd75c89
hernan.cussi <hernan.cussi@globallogic.com>
2012-10-23 12:15:59 -0300

    Agrego requireJS y encapsulamiento

--- todolist-parent/todolist-web/src/main/webapp/backbone/js/collections/users.js
+++ todolist-parent/todolist-web/src/main/webapp/backbone/js/collections/users.js
@@ -1,30 +1,40 @@
-//var app = app || {};
-//
-//(function() {
-//	'use strict';
+(function(context) {
+	'use strict';
 
-	// User Collection
-	// ---------------
-
-	// The collection of users is backed by *localStorage* instead of a remote
-	// server.
-window.UserList = Backbone.Collection.extend({
-
-		// Reference to this collection's model.
-		model: User,
-
-		url:"../rest/users",
+	define([
+	        'jquery',
+	        'underscore',
+	        'backbone',
+	        'utils',
+	        'models/user'
+	    ], function($,_,Backbone){
 		
-		// Filter down the list of all todo items that are finished.
-		otherUsers: function() {
-			return this.filter(function( todo ) {
-				return true; // todo.get('id'); != user in session
-			});
-		}
-
-	});
+		// User Collection
+		// ---------------
+		
+		// The collection of users is backed by *localStorage* instead of a remote
+		// server.
+		context.UserList = Backbone.Collection.extend({
+			
+			// Reference to this collection's model.
+			model: context.User,
+			
+			url:"../rest/users",
+			
+			// Filter down the list of all todo items that are finished.
+			otherUsers: function() {
+				return this.filter(function( todo ) {
+					return true; // todo.get('id'); != user in session
+				});
+			}
+		
+		});
 //
 //	// Create our global collection of **UserList**.
 //	app.UserList = new UserList();
+		
+		
+	});
+	
 //
-//}());
+}(app));
