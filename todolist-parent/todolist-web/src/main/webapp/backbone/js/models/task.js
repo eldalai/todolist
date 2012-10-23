1d9f7c549ea0d5d53130942db50b4204afec2dce
hernan.cussi <hernan.cussi@globallogic.com>
2012-10-23 12:17:03 -0300

    Agrego requireJS y encapsulamiento

--- todolist-parent/todolist-web/src/main/webapp/backbone/js/models/task.js
+++ todolist-parent/todolist-web/src/main/webapp/backbone/js/models/task.js
@@ -1,24 +1,31 @@
-//var app = app || {};
-//
-//(function() {
-//	'use strict';
+(function(context) {
+	'use strict';
 
-	// Todo Model
-	// ----------
-
-	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
-	window.Task = Backbone.Model.extend({
+	define([
+	        'jquery',
+	        'underscore',
+	        'backbone'
+	    ], function($,_,Backbone){
 		
-		urlRoot:"../rest/tasks",
-		// Default attributes for the todo
-		// and ensure that each todo created has `title` and `completed` keys.
-		defaults: {
-			id: null,
-			title: '',
-			taskStatus: '', // PENDING, DONE
-			taskType: '' // NORMAL, URGENT 
-		}
-
+		// Todo Model
+		// ----------
+		
+		// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+		context.Task = Backbone.Model.extend({
+			
+			urlRoot:"../rest/tasks",
+			// Default attributes for the todo
+			// and ensure that each todo created has `title` and `completed` keys.
+			defaults: {
+				id: null,
+				title: '',
+				taskStatus: '', // PENDING, DONE
+				taskType: '' // NORMAL, URGENT 
+			}
+		});
+		
+		
 	});
-//
-//}());
+
+
+}(app));
