0adbeaee2d6aaf63196bc07be7d6ca0af781becd
hernan.cussi <hernan.cussi@globallogic.com>
2012-10-29 15:14:13 -0300

    Added 'use strict' explanation for modern browsers

--- todolist-parent/todolist-web/src/main/webapp/backbone/js/views/contact.js
+++ todolist-parent/todolist-web/src/main/webapp/backbone/js/views/contact.js
@@ -1,4 +1,13 @@
 (function(context) {
+	
+	/*
+	 
+	  Removes features from the language, making the programs simpler and less error-prone.
+	  The strict mode is triggered by an ordinary string, which older implementations of the language simply ignore.
+	  This means that the usage of strict mode is backward compatible, because it won’t raise errors in older browsers that don’t understand it.
+	  
+	 */
+	
 	'use strict';
 	
 	define([
