6e6b2a53f0cb2b47450e619eb93f900264c849d5
gabriel <eldalai@gmail.com>
2012-11-19 11:45:41 -0300

    namespace function

--- todolist-parent/todolist-web/src/main/webapp/backbone/js/utils.js
+++ todolist-parent/todolist-web/src/main/webapp/backbone/js/utils.js
@@ -18,9 +18,23 @@
 				i = new context[clazz]();
 				instances[clazz] = i;
 			}
-            i.model = model;
+            i.model = model; 
 			i.render();
 		};
 	});
 	
 }(app));
+
+function namespace(namespaceString) {
+    var parts = namespaceString.split('.'),
+        parent = window,
+        currentPart = '';    
+        
+    for(var i = 0, length = parts.length; i < length; i++) {
+        currentPart = parts[i];
+        parent[currentPart] = parent[currentPart] || {};
+        parent = parent[currentPart];
+    }
+    
+    return parent;
+}
